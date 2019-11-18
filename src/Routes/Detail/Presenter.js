f90ce6bc943d2c21922a27941e86933b868a8b18
minhan <alsgks1218@gmail.com>
2019-11-18 23:42:41 +0900

    completed 6-3

--- src/Routes/Detail/Presenter.js
+++ src/Routes/Detail/Presenter.js
@@ -1,1 +1,13 @@
-export default () => "Detail";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Presenter = ({ result, error, loading }) => null;
+
+Presenter.propTypes = {
+  result: PropTypes.object,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string
+};
+
+export default Presenter;
